a649222e4055d0eb3d3de9c327aeacaf5d28bab9
Somnid <>
2014-07-28 20:40:36 -0600

    Messing around with load timing

--- js/cjax-loader.js
+++ js/cjax-loader.js
@@ -1,10 +1,10 @@
-(function(){
+document.addEventListener("DOMContentLoaded", function(){
   var script = document.createElement("script");
   script.src = chrome.extension.getURL("js/cjax.js");
   var identifier = document.createElement("span");
   identifier.style.display = "hidden";
   identifier.innerText = chrome.runtime.id;
   identifier.id = "cjax-ext-id";
-  document.body.appendChild(identifier);
-  document.body.appendChild(script);
-})();
+  document.head.appendChild(identifier);
+  document.head.appendChild(script);
+});
